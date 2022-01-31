e3ffda61537ec21f1f63f09cb4a7b956dc637f4d
Stephen Lyons Jr <stephen@lyonsden.com>
2022-01-31 17:36:48 -0500

    skeleton

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+
+alert('Hello world');
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+// cntr+k=+c for comment cntr+k+u for uncomment
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
+let favoriteFood = 'Lobster';
+document.write(favoriteFood);
