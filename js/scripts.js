6a862e6dee215369adcc28fc6b65a5f849b53f0c
Stephen Lyons Jr <stephen@lyonsden.com>
2022-02-03 19:20:03 -0500

    fixing syntax

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 let pokemonList = [
-    {name: Bulbasaur, height: 0.7m, type: ['grass', 'poison']}
-    {name: Ivysaur, height: 1.0m, type: ['grass', 'poison']}
-    {name: Venusaur, height: 2.0m, type: ['grass', 'poison']}
-]
+    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
+    {name: 'Ivysaur', height: 1, type: ['grass', 'poison']},
+    {name: 'Venusaur', height: 2, type: ['grass', 'poison']}
+    
+];
