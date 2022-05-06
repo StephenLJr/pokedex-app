d6644aea8249ad4a8d4e32526da844b67d7fa169
Stephen Lyons Jr <stephen@lyonsden.com>
2022-05-06 17:45:01 -0400

    added loop for writing data in HTML

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,11 @@ let pokemonList = [
     {name: 'Wartortle', height: 1, type: 'water'},
     {name: 'Blastoise', height: 1.6, type: 'water'},
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1.6) { //highlighting large pokemon
+        document.write('<p class="large">' + <strong>pokemonList[i].name</strong>, '(height: ' = pokemonList[i].height + ')-Wow, that\’s big!</p>')
+    } else{
+        document.write('<p>' + <strong>pokemonList[i].name)</strong>, '(height: ' + pokemonList[i].height + ')</p>')
+    }
+}
