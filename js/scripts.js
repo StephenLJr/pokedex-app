fbac3b707d36654a3608085df856d4bb7c1fb911
Stephen Lyons Jr <stephen@lyonsden.com>
2022-07-19 13:11:56 -0400

    IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,43 @@ pokemonList.forEach(function(pokemon) {
         document.write('<p>' + '<strong>' + pokemon.name + '</strong>' + ', (height: ' + pokemon.height + ')</p>')
     }
 });
+
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
+        {name: 'Ivysaur', height: 1, type: ['grass', 'poison']},
+        {name: 'Venusaur', height: 2, type: ['grass', 'poison']},
+        {name: 'Charmander', height: 0.6, type: 'fire'},
+        {name: 'Charmeleon', height: 1.1, type: 'fire'},
+        {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
+        {name: 'Squirtle', height: 0.5, type: 'water'},
+        {name: 'Wartortle', height: 1, type: 'water'},
+        {name: 'Blastoise', height: 1.6, type: 'water'},
+    ]
+
+    return {
+        add: function (pokemon) {
+            pokemonList.push(pokemon);
+        },
+        getAll: function () {
+            return pokemonList;
+        }
+    };
+})();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({
+    name: 'Caterpie', height: 0.3, type: 'bug'});
+    console.log(pokemonRepository.getAll());
+
+
+
+
+
+
+
+
+
 function divide(dividend, divisor){
     if(divisor === 0){
         return "You're trying to divide by zero."
