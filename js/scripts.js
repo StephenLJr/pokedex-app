40892acebe09540eaf5178a8aa9eb1410b68257a
Stephen Lyons Jr <stephen@lyonsden.com>
2022-08-29 05:57:58 -0400

    fixing repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,3 @@
-let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
-    {name: 'Ivysaur', height: 1, type: ['grass', 'poison']},
-    {name: 'Venusaur', height: 2, type: ['grass', 'poison']},
-    {name: 'Charmander', height: 0.6, type: 'fire'},
-    {name: 'Charmeleon', height: 1.1, type: 'fire'},
-    {name: 'Charizard', height: 1.7, type: ['fire', 'flying']},
-    {name: 'Squirtle', height: 0.5, type: 'water'},
-    {name: 'Wartortle', height: 1, type: 'water'},
-    {name: 'Blastoise', height: 1.6, type: 'water'},
-]
-
 let pokemonRepository = (function () {
     let pokemonList = [
         {name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']},
@@ -30,13 +18,13 @@ let pokemonRepository = (function () {
           "height" in pokemon &&
           "types" in pokemon
         ) {
-          repository.push(pokemon);
+          pokemonList.push(pokemon);
         } else {
           console.log("pokemon is not correct");
         }
       }
       function getAll() {
-        return repository;
+        return pokemonList;
       }
       function addListItem(pokemon){
         let PokemonList = document.querySelector(".pokemon-list");
